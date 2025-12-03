fff12e72e6d1bae169faf14bf917f0828ea711ee
Mariana Lerma <92741609+marianalero@users.noreply.github.com>
2025-12-02 17:11:35 -0700

    changes

--- src/constants/routes.tsx
+++ src/constants/routes.tsx
@@ -95,7 +95,6 @@ const routes: RouteConfig[] = [
   {path:'/bridalshower-sophia', element: <BSSophia></BSSophia>},
   {path:'/save-date-xv-dalia', element: <STDDalia></STDDalia>},
    {path:'/baby-shower-alec', element: <BabyShowerAlec></BabyShowerAlec>},
-   {path:'/prev-ptdr', element: <PosataTDR></PosataTDR>},
    {path:'/pos-tdr', element: <PosataTDR></PosataTDR>},
   //Borradores
   {path:'/prev-kd-3', element: <WeddingKorinaDaniel></WeddingKorinaDaniel>},
