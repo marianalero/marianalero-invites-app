b5c62e22050f0f5aa9957024530c78e6e806c255
Mariana Lerma <92741609+marianalero@users.noreply.github.com>
2026-06-02 12:24:34 -0700

    add blur

--- src/components/Intro/InvitationIntro/InvitationIntro.tsx
+++ src/components/Intro/InvitationIntro/InvitationIntro.tsx
@@ -1,5 +1,5 @@
 import { Box, Typography } from "@mui/material";
-import { RefObject } from "react";
+import { RefObject, useEffect, useState } from "react";
 import { MusicFabPlayerHandle } from "../../MusicFabPlayer/MusicFabPlayer";
 import { Fade } from "react-awesome-reveal";
 
@@ -106,11 +106,25 @@ const InvitationIntro = ({
     height: "70px",
   },
 }: InvitationIntroProps) => {
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+        if (!envelopeImg) return;
+  
+          const img = new Image();
+          img.src = envelopeImg;
+  
+          img.onload = () => setLoaded(true);
+      }, [envelopeImg]);
+
   const handleEnter = () => {
     musicRef?.current?.play();
     onEnter();
   };
 
+  const GENERIC_BLUR =
+    "data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAkGBxAQEBUQEA8PEA8PDw8PDw8PDw8PDw8PFREWFhURFRUYHSggGBolGxUVITEhJSkrLi4uFx8zODMtNygtLisBCgoKDg0OFxAQFy0dHR0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLf/AABEIAAEAAQMBIgACEQEDEQH/xAAXAAEBAQEAAAAAAAAAAAAAAAABAgAD/8QAFhABAQEAAAAAAAAAAAAAAAAAAAER/9oADAMBAAIQAxAAAAH6A//EABgQAQEBAQEAAAAAAAAAAAAAAAERAhIh/9oACAEBAAEFAk8d4o//xAAWEQEBAQAAAAAAAAAAAAAAAAAAARH/2gAIAQMBAT8BSP/EABURAQEAAAAAAAAAAAAAAAAAAAAR/9oACAECAQE/ASf/xAAbEAADAQEBAQEAAAAAAAAAAAABERAhMUFRcf/aAAgBAQAGPwKzK0kAq0p1k//EABsQAQEAAwEBAQAAAAAAAAAAAAERACExQVFh/9oACAEBAAE/IVFZfE2PqC5nSlQ2RZ5WqX//2gAMAwEAAgADAAAAEB//xAAWEQEBAQAAAAAAAAAAAAAAAAAAARH/2gAIAQMBAT8QEf/EABURAQEAAAAAAAAAAAAAAAAAAAAR/9oACAECAQE/ECL/xAAbEAEBAQEAAwEAAAAAAAAAAAABEQAhMVFhcf/aAAgBAQABPxDkXIpT+R9xKk4a5QZ2h+V5J7VZ//Z";
+    
   return (
     <Box
       sx={{
@@ -237,18 +251,22 @@ const InvitationIntro = ({
               }}
             >
               <img
-                src={envelopeImg}
+                src={ loaded ? envelopeImg : GENERIC_BLUR}  
                 alt="Envelope"
+                loading="eager"
+                decoding="async"
                 style={{
                   width: "100%",
                   objectFit: "contain",
                   display: "block",
+                  filter: loaded ? "blur(0)" : "blur(24px)",
+                  opacity: loaded ? 1 : 0.85,
                 }}
               />
 
               {topLeftCornerImg && (
                 <img
-                  src={topLeftCornerImg}
+                  src={ loaded ? topLeftCornerImg : GENERIC_BLUR}
                   alt="Top left decoration"
                   style={{
                     position: "absolute",
@@ -260,13 +278,15 @@ const InvitationIntro = ({
                     objectFit: "contain",
                     zIndex: 2,
                     pointerEvents: "none",
+                    filter: loaded ? "none" : "blur(20px)",
+                    opacity: loaded ? 1 : 0,
                   }}
                 />
               )}
 
               {bottomRightCornerImg && (
                 <img
-                  src={bottomRightCornerImg}
+                  src={ loaded ? bottomRightCornerImg : GENERIC_BLUR}
                   alt="Bottom right decoration"
                   style={{
                     position: "absolute",
@@ -278,6 +298,8 @@ const InvitationIntro = ({
                     objectFit: "contain",
                     zIndex: 2,
                     pointerEvents: "none",
+                    filter: loaded ? "none" : "blur(20px)",
+                    opacity: loaded ? 1 : 0,
                   }}
                 />
               )}
@@ -285,7 +307,7 @@ const InvitationIntro = ({
               {/* Sello */}
               {sealImg && (
                 <img
-                  src={sealImg}
+                  src={ loaded ? sealImg : GENERIC_BLUR}
                   alt="Seal"
                   style={{
                     position: "absolute",
@@ -302,6 +324,8 @@ const InvitationIntro = ({
 
                     objectFit: "contain",
                     zIndex: 3,
+                    filter: loaded ? "none" : "blur(20px)",
+                    opacity: loaded ? 1 : 0,
                   }}
                 />
               )}
